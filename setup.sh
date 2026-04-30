12ccc62c60b5ea6f2f71d1eab0307fe058353d29
ealexandrohin <ealexandrohin@gmail.com>
2026-04-30 14:40:12 +0300

    add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+(
+  cd .deps/ || exit
+  makepkg -si
+)
+
+(
+  cd yay-bin || exit
+  makepkg -si
+)
+
+(
+  cd .aur/ || exit
+  yay -Bi .
+)
+
+(
+  sudo mv /etc/ly/config.ini /etc/ly/config.ini.backup
+  sudo ln -s ~/.config/ly/config.ini /etc/ly/config.ini
+)
+
+(
+  sudo cp /etc/vconsole.conf /etc/vconsole.conf.backup
+  awk -F= 'NR==FNR{a[$1]=$0;next} $1 in a{print a[$1];delete a[$1];next} {print} END{for(k in a)print a[k]}' ~/.config/ly/vconsole.conf /etc/vconsole.conf >/tmp/vconsole.conf && sudo mv /tmp/vconsole.conf /etc/vconsole.conf
+  sudo mkinitcpio -P
+)
