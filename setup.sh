32e70a1ceaac699f87f17dfafd0ba366cbfdb4b8
ealexandrohin <ealexandrohin@gmail.com>
2026-05-02 18:41:48 +0300

    upd(mimeapps): add alacritty-open and setup.sh

--- setup.sh
+++ setup.sh
@@ -23,3 +23,8 @@
   awk -F= 'NR==FNR{a[$1]=$0;next} $1 in a{print a[$1];delete a[$1];next} {print} END{for(k in a)print a[k]}' ~/.config/ly/vconsole.conf /etc/vconsole.conf >/tmp/vconsole.conf && sudo mv /tmp/vconsole.conf /etc/vconsole.conf
   sudo mkinitcpio -P
 )
+
+(
+  sudo cp /usr/share/applications/Alacritty.desktop /usr/share/applications/Alacritty-open.desktop
+  sudo sed -i "s/^Exec=alacritty$/Exec=alacritty --working-directory %U/" /usr/share/applications/Alacritty-open.desktop
+)
