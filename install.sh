c7c1fb4b90f2b09e4050e62a43a3354ea3816031
ealexandrohin <ealexandrohin@gmail.com>
2026-07-29 18:51:37 +0300

    add: install.sh, setup.sh, work.sh

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+(
+  cd .deps/ || exit
+  makepkg -si
+)
+
+(
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin || exit
+  makepkg -si
+
+  yay -Y --gendb
+  yay -Syu --devel
+  yay -Y --devel --save
+)
+
+(
+  cd .aur/ || exit
+  yay -Bi .
+)
+
+(
+  sudo mv /etc/ly/config.ini /etc/ly/config.ini.backup
+  sudo ln -s ~/.config/ly/config.ini /etc/ly/config.ini
+)
+
+(
+  sudo cp /etc/vconsole.conf /etc/vconsole.conf.backup
+  awk -F= 'NR==FNR{a[$1]=$0;next} $1 in a{print a[$1];delete a[$1];next} {print} END{for(k in a)print a[k]}' ~/.config/ly/vconsole.conf /etc/vconsole.conf >/tmp/vconsole.conf
+  sudo mv /tmp/vconsole.conf /etc/vconsole.conf
+  sudo mkinitcpio -P
+)
+
+(
+  sudo cp /usr/share/applications/Alacritty.desktop /usr/share/applications/Alacritty-open.desktop
+  sudo sed -i "s/^Exec=alacritty$/Exec=alacritty --working-directory %U/" /usr/share/applications/Alacritty-open.desktop
+)
+
+(
+  sudo mkdir /hdd
+  sudo mkdir /usb
+  sudo mkdir /ventoy
+  sudo mkdir /windows
+
+  sudo groupadd shared
+  sudo chown :shared /hdd /usb /ventoy /windows
+  sudo chmod g+rwx /hdd /usb /ventoy /windows
+)
+
+(
+  sudo systemctl enable --now systemd-resolved
+  sudo systemctl enable --now v2raya
+)
---
+++ work.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(
+  sudo openvpn3-admin init-config
+)
